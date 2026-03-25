9ea2c296da2cd358a0ac1d7df3ac041a52ba1a11
contactfiregrade-stack <contact.firegrade@gmail.com>
2026-03-25 09:16:07 +0100

    Enable React strict mode in Next.js config
    Removed redundant Next.js configuration and enabled strict mode.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+
+module.exports = nextConfig;/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = nextConfig;
